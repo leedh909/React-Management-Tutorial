29e553863352bdad9e5fa811baffdf0238e2256e
이 대희 <null>
2021-04-01 16:19:05 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
-import Customer from './components/Customer'
+import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import './App.css';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -26,9 +27,23 @@ const styles = theme =>({
 
 class App extends Component{
 
-  state = {
-    customers:[],
-    completed: 0
+  constructor(props){
+    super(props);
+    this.state = {
+      customers:'',
+      completed:0
+    }
+  }
+
+  stateRefresh = () => {
+    this.setState({
+      customers:'',
+      completed: 0
+    });
+
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
   }
 
   componentDidMount(){
@@ -41,9 +56,9 @@ class App extends Component{
 
   callApi = async () => {
     const response = await fetch('/api/customers');
-    console.log("response: ",response);
+    // console.log("response: ",response);
     const body = await response.json();
-    console.log("body: ",body);
+    // console.log("body: ",body);
     return body;
   }
 
@@ -55,43 +70,46 @@ class App extends Component{
   render(){
     const { classes } = this.props;
     return(
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {
-              this.state.customers ? 
-              this.state.customers.map( c => {
-                return (
-                  <Customer 
-                    key={c.id}
-                    id={c.id}
-                    image={c.image}
-                    name={c.name}
-                    birthday={c.birthday}
-                    gender={c.gender}
-                    job={c.job}
-                  />
-                );
-              }) : 
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
               <TableRow>
-                <TableCell colSpan="6" align ="center">
-                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
-                </TableCell>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
               </TableRow>
-            }
-          </TableBody>
-        </Table>        
-      </Paper>
+            </TableHead>
+            <TableBody>
+              {
+                this.state.customers ? 
+                this.state.customers.map( c => {
+                  return (
+                    <Customer 
+                      key={c.id}
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    />
+                  );
+                }) : 
+                <TableRow>
+                  <TableCell colSpan="6" align ="center">
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                  </TableCell>
+                </TableRow>
+              }
+            </TableBody>
+          </Table>        
+        </Paper>
+        <CustomerAdd stateRefresh={this.stateRefresh} />
+      </div>
     );
   } 
 }
--- server.js
+++ server.js
@@ -12,12 +12,15 @@ const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
 const mssql = require('mssql');
 
+const multer = require('multer');
+const upload =multer({dest: './upload'})
+
 app.get('/api/customers',(req,res)=>{
     mssql.connect(conf, function(err){
         if(err) console.log(err);
 
         var request = new mssql.Request();
-        request.query('select * from customer2', function(err, result){
+        request.query('select * from customer', function(err, result){
             if(err) console.log(err);
             // console.log("result: ",result);
             res.send(result.recordset);
@@ -25,6 +28,41 @@ app.get('/api/customers',(req,res)=>{
     })
     
 });
+
+app.use('/image',express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req,res) =>{
+    let sql = "INSERT INTO CUSTOMER VALUES (@image, @name, @birthday, @gender, @job)";
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
+
+    // console.log(image);
+    // console.log(name);
+    // console.log(birthday);
+    // console.log(gender);
+    // console.log(job);
+
+    mssql.connect(conf, function(err){
+        if(err) console.log(err);
+
+        var request = new mssql.Request();
+        request.input('image',mssql.VarChar, image);
+        request.input('name',mssql.NVarChar, name);
+        request.input('birthday',mssql.VarChar, birthday);
+        request.input('gender',mssql.NVarChar, gender);
+        request.input('job',mssql.NVarChar, job);
+        request.query(sql, function(err, result){
+            if(err) console.log(err);
+            // console.log("result: ",result);
+            res.send(result.recordset);
+            console.log(err);
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
